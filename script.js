4a861c8d4e8ed55b37adebc600cf68878e24c8c0
GabrielNScepanski <161836677+GabrielNScepanski@users.noreply.github.com>
2024-10-07 10:37:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let nomes = ['Amir', 'bemir', 'cemir']
+console.log(nome.includes('bemir'))
+console.log(nome.includes('AMIR'))
+
+if(nomes.includes('amir')){
+    console.log('Nome encontrado')
+}else{
+    console.log('Nome mão encoantrado')
+}
+
+let aluno='albano'
+aluno.startsWith('alb')
+console.log(aluno.startsWith('Alb'))
+console.log(aluno.startsWith('alb'))
+
+console.log(aluno.endsWith('ano'))
+console.log(aluno.endsWith('anO'))
